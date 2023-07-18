5159b051433fb0a7b80a0ee148b18fec635f3d9b
Natanael Coladello <ncoladello@gmail.com>
2023-07-18 20:00:47 +0200

    refactor: refactored file removing code 'n tunning db connection

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,21 +1,46 @@
+import {useState, useEffect} from 'react' ;
 import { useParams } from "react-router-dom";
+import axios from 'axios';
 import articles from "./article-content";
-
-import React from "react";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList from '../components/CommentsList';
 
 const ArticlePage = () => {
+
+const [articleInfo, setArticleInfo]= useState({upvotes:0, comments: [],});
+
+useEffect(()=>{
+
+})
+
   const { articleId } = useParams();
+  
+  useEffect( ()=>{
+    const loadArticleInfo = async ()=> {
+      const response = await axios.get(`/api/articles/${articleId}`)
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    }
+
+    loadArticleInfo();
+
+  },[])
+  
   const article = articles.find((article) => article.name === articleId);
+  
+
 
   if (!article) return <NotFoundPage />;
 
   return (
     <section>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s)</p>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
+
+      <CommentsList comments={articleInfo.comments}/>
     </section>
   );
 };
