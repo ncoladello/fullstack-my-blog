6ad991992e82a83e2da95ad3bb0500d455fea561
Natanael Coladello <ncoladello@gmail.com>
2023-07-18 20:49:59 +0200

    fit: added useStage, upvote function and AddCommentForm component

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,46 +1,50 @@
-import {useState, useEffect} from 'react' ;
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
-import CommentsList from '../components/CommentsList';
+import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
-const [articleInfo, setArticleInfo]= useState({upvotes:0, comments: [],});
-
-useEffect(()=>{
-
-})
+  useEffect(() => {});
 
   const { articleId } = useParams();
-  
-  useEffect( ()=>{
-    const loadArticleInfo = async ()=> {
-      const response = await axios.get(`/api/articles/${articleId}`)
+
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
       const newArticleInfo = response.data;
       setArticleInfo(newArticleInfo);
-    }
+    };
 
     loadArticleInfo();
+  }, []);
 
-  },[])
-  
   const article = articles.find((article) => article.name === articleId);
-  
 
+  const addUpvote = async () => {
+    const response = await axios.put(`/api/articles/${articleId}/upvote`);
+    const updatedArticle = response.data;
+    setArticleInfo(updatedArticle);
+  };
 
   if (!article) return <NotFoundPage />;
 
   return (
     <section>
       <h1>{article.title}</h1>
-      <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      <div className="upvotes-section">
+        <button onClick={addUpvote}>Upvote</button>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      </div>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
-
-      <CommentsList comments={articleInfo.comments}/>
+      <AddCommentForm articleName={articleId} onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}/>
+      <CommentsList comments={articleInfo.comments} />
     </section>
   );
 };
