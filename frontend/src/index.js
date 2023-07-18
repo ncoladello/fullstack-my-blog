c1e84410ac463466144113e39739fe931f047a39
Natanael Coladello <ncoladello@gmail.com>
2023-07-18 20:47:39 +0200

    add: firebase configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBojavBFN8eNt5vGjMRC9Fz01kuqXOlYqY",
+  authDomain: "fullstack-blog-3d698.firebaseapp.com",
+  projectId: "fullstack-blog-3d698",
+  storageBucket: "fullstack-blog-3d698.appspot.com",
+  messagingSenderId: "457921651490",
+  appId: "1:457921651490:web:3e8307465db8fcf4f97b7b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
