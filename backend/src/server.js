0f3db538cbc45752d6624f535d65d22b51957ad1
Natanael Coladello <ncoladello@gmail.com>
2023-07-22 13:00:38 +0200

    feat: Add authentication middleware and upvote feature
    Implemented authentication middleware to validate the 'authToken' from request headers and assign 'req.user' if valid. If the token is invalid or missing, the middleware will respond with a 400 status code.
    
    Also, added the ability to upvote articles based on the user's ID ('uid'). The 'canUpvote' property of an article is set to 'true' if the user is authenticated and hasn't already upvoted the article.
    
    Both features aim to enhance user interactions and ensure the integrity of upvote actions.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,37 +1,50 @@
-import fs from 'fs';
-import admin from 'firebase-admin';
+import fs from "fs";
+import admin from "firebase-admin";
 import express from "express";
-import{db, connectToDb} from './db.js';
+import { db, connectToDb } from "./db.js";
 
-const credentials = JSON.parse(fs.readFileSync('../credentials.json'));
+const credentials = JSON.parse(fs.readFileSync("../credentials.json"));
 
 admin.initializeApp({
   credential: admin.credential.cert(credentials),
-})
-
+});
 
 const app = express();
 app.use(express.json());
 
+app.use(async (req, res, next) => {
+  const { authToken } = req.headers;
+
+  if (authToken) {
+    try {
+      req.user = await admin.auth().verifyIdToken(authToken);
+    } catch (e) {
+      res.sendStatus(400);
+    }
+  }
+  next();
+});
+
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
 
+  const{uid} = req.user; 
+
   const article = await db.collection("articles").findOne({ name });
 
-
   if (article) {
+
+
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpvote = uid &&!upvoteIds.includes(uid);  
     res.json(article);
   } else {
     res.sendStatus(404);
   }
-
-
 });
 
 app.put("/api/articles/:name/upvote", async (req, res) => {
   const { name } = req.params;
-
-  
 
   await db.collection("articles").updateOne(
     { name },
@@ -53,7 +66,6 @@ app.post("/api/articles/:name/comments", async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
 
-
   await db.collection("articles").updateOne(
     { name },
     {
@@ -61,7 +73,7 @@ app.post("/api/articles/:name/comments", async (req, res) => {
     }
   );
 
-  const article = await db.collection('articles').findOne({name});
+  const article = await db.collection("articles").findOne({ name });
 
   if (article) {
     res.json(article);
@@ -70,11 +82,9 @@ app.post("/api/articles/:name/comments", async (req, res) => {
   }
 });
 
-connectToDb(()=>{
-  console.log('Successfully connected to database!')
+connectToDb(() => {
+  console.log("Successfully connected to database!");
   app.listen(8000, () => {
     console.log("Server is listening on port 8000");
   });
-  
-})
-
+});
