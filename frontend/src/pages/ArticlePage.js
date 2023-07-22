6e840a691ec02e11e199dac641311989e95a61a2
Natanael Coladello <ncoladello@gmail.com>
2023-07-22 11:35:57 +0200

    feat: add user login verification for comments and upvotes

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -5,11 +5,12 @@ import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
+import useUser from "../hooks/useUser";
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
-  useEffect(() => {});
+  const { user, isLoading } = useUser();
 
   const { articleId } = useParams();
 
@@ -37,13 +38,25 @@ const ArticlePage = () => {
     <section>
       <h1>{article.title}</h1>
       <div className="upvotes-section">
-        <button onClick={addUpvote}>Upvote</button>
+        {user ? (
+          <button onClick={addUpvote}>Upvote</button>
+        ) : (
+          <button>Log in to upvote</button>
+        )}
         <p>This article has {articleInfo.upvotes} upvote(s)</p>
       </div>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
-      <AddCommentForm articleName={articleId} onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}/>
+
+      {user ? (
+        <AddCommentForm
+          articleName={articleId}
+          onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+        />
+      ) : (
+        <button>Log in to comment</button>
+      )}
       <CommentsList comments={articleInfo.comments} />
     </section>
   );
